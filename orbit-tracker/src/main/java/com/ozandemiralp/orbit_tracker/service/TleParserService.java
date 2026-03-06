90301f5c8fd885199254b6a4dd86c1ed4870b5ac
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-03-06 22:53:21 +0300

    feat: add tle parser service

--- orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/service/TleParserService.java
+++ orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/service/TleParserService.java
@@ -1,4 +1,36 @@
 package com.ozandemiralp.orbit_tracker.service;
 
+import com.ozandemiralp.orbit_tracker.dto.SatelliteDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TleParserService {
+
+    public List<SatelliteDTO> parseTleResponse(String rawTle){
+        List<SatelliteDTO> satellites = new ArrayList<>();
+
+        String[] lines = rawTle.split("\\r?\\n");
+
+        for (int i = 0; i < lines.length; i++) {
+            if(i + 2 < lines.length){
+                String name = lines[i].trim();
+                String line1 = lines[i+1];
+                String line2 = lines[i+2];
+
+                satellites.add(new SatelliteDTO(name, line1, line2));
+            }
+        }
+        return satellites;
+    }
+
+    public SatelliteDTO findSatelliteByName(List<SatelliteDTO> list, String name){
+        return list.stream()
+                .filter(s -> s.name().toUpperCase().contains(name.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Satellite not found" + name));
+    }
 }
+
