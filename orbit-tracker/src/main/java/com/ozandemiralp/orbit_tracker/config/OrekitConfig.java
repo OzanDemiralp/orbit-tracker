f2f5b88c4443dfb89a5665a3e97535ec344db610
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-03-07 01:50:38 +0300

    fix: fix orekit data config

--- orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/config/OrekitConfig.java
+++ orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/config/OrekitConfig.java
@@ -2,26 +2,19 @@ package com.ozandemiralp.orbit_tracker.config;
 
 import jakarta.annotation.PostConstruct;
 import org.orekit.data.DataContext;
-import org.orekit.data.DataProvidersManager;
 import org.orekit.data.DirectoryCrawler;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 
 @Configuration
 public class OrekitConfig {
-
-    @Value("${orekit.data.path:src/main/resources/orekit-data-main.orekit-data}")
-    private String orekitDataPath;
-
     @PostConstruct
-    public void init() {
-        File orekitData = new File(orekitDataPath);
-        if (!orekitData.exists()) {
-            throw new RuntimeException("Orekit data not found at " + orekitDataPath);
-        }
-        DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
-        manager.addProvider(new DirectoryCrawler(orekitData));
+    public void initOrekit() {
+        File orekitData = new File("orbit-tracker/src/main/resources/orekit-data-main");
+        DataContext.getDefault().getDataProvidersManager().clearProviders();
+        DataContext.getDefault()
+                .getDataProvidersManager()
+                .addProvider(new DirectoryCrawler(orekitData));
     }
 }
