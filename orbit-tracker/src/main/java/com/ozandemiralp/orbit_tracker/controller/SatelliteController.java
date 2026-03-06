d5eb417bb9130fb9e69fa229ae824d474c613482
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-03-07 01:51:54 +0300

    feat: add satelliteTle endpoint

--- orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/controller/SatelliteController.java
+++ orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/controller/SatelliteController.java
@@ -1,4 +1,30 @@
 package com.ozandemiralp.orbit_tracker.controller;
 
+import com.ozandemiralp.orbit_tracker.client.CelestrakClient;
+import com.ozandemiralp.orbit_tracker.dto.SatelliteDTO;
+import com.ozandemiralp.orbit_tracker.dto.SatelliteTleRequestDTO;
+import com.ozandemiralp.orbit_tracker.service.TleParserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/satellites")
+@AllArgsConstructor
 public class SatelliteController {
+
+    private final TleParserService tleParserService;
+    private final CelestrakClient celestrakClient;
+
+    @PostMapping("/satelliteTle")
+    public Mono<SatelliteDTO> getSatelliteTle(@RequestBody SatelliteTleRequestDTO requestDTO){
+        return celestrakClient.getTleDataByGroup(requestDTO.group())
+                .map(rawTle -> {
+                    List<SatelliteDTO> allSatellites = tleParserService.parseTleResponse(rawTle);
+                    return tleParserService.findSatelliteByName(allSatellites, requestDTO.satelliteName());
+                });
+        }
+
 }
