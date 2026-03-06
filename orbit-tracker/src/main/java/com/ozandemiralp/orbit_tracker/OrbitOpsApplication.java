1b242ffed3bd0f57b596b29f2eda75db82834db5
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-03-06 21:44:40 +0300

    feat: initial commit with spring boot and orekit dependencies

---
+++ orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/OrbitOpsApplication.java
@@ -0,0 +1,13 @@
+package com.ozandemiralp.orbit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitOpsApplication.class, args);
+	}
+
+}
---
+++ orbit-tracker/src/test/java/com/ozandemiralp/orbit_tracker/OrbitOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozandemiralp.orbit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
